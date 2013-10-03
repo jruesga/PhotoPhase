1e351a83afac3dc631bb7c47544459f87128766f
Jorge Ruesga <jorge@ruesga.com>
2013-10-04 00:15:11 +0200

    Replace Cid image
    Signed-off-by: Jorge Ruesga <jorge@ruesga.com>

--- src/com/ruesga/android/wallpapers/photophase/shapes/OopsShape.java
+++ src/com/ruesga/android/wallpapers/photophase/shapes/OopsShape.java
@@ -136,7 +136,7 @@ public class OopsShape implements DrawableShape {
         mMessage = ctx.getString(resourceMessageId);
 
         // Load the textures
-        mOopsImageTexture = GLESUtil.loadTexture(ctx, R.drawable.bg_cid_oops, null, null, false);
+        mOopsImageTexture = GLESUtil.loadTexture(ctx, R.drawable.bg_oops, null, null, false);
         Bitmap textBitmap = text2Bitmap(ctx, mMessage);
         mOopsTextTexture = GLESUtil.loadTexture(textBitmap, null, null);
 
