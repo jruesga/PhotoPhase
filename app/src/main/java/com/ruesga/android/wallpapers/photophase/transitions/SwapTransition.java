0936fdeb1b4c15130cb616135ba5359ca4d1a427
Jorge Ruesga <jorge@ruesga.com>
2016-05-10 23:13:18 +0200

    Remove Swap transition's inheritance from Null transition
    Signed-off-by: Jorge Ruesga <jorge@ruesga.com>

--- app/src/main/java/com/ruesga/android/wallpapers/photophase/transitions/SwapTransition.java
+++ app/src/main/java/com/ruesga/android/wallpapers/photophase/transitions/SwapTransition.java
@@ -17,19 +17,28 @@
 package com.ruesga.android.wallpapers.photophase.transitions;
 
 import android.content.Context;
+import android.opengl.GLES20;
 
+import com.ruesga.android.wallpapers.photophase.PhotoFrame;
+import com.ruesga.android.wallpapers.photophase.R;
 import com.ruesga.android.wallpapers.photophase.textures.TextureManager;
 import com.ruesga.android.wallpapers.photophase.transitions.Transitions.TRANSITIONS;
+import com.ruesga.android.wallpapers.photophase.utils.GLESUtil;
+
+import java.nio.FloatBuffer;
 
 /**
  * A simple transition that swap an image after the transition time is ended.
  */
-public class SwapTransition extends NullTransition {
+public class SwapTransition extends Transition {
+
+    private static final int[] VERTEX_SHADER = {R.raw.default_vertex_shader};
+    private static final int[] FRAGMENT_SHADER = {R.raw.default_fragment_shader};
 
     private static final float TRANSITION_TIME = 250.0f;
 
     public SwapTransition(Context ctx, TextureManager tm) {
-        super(ctx, tm);
+        super(ctx, tm, VERTEX_SHADER, FRAGMENT_SHADER);
     }
 
     @Override
@@ -57,4 +66,55 @@ public class SwapTransition extends NullTransition {
         draw(delta < 1f ? mTransitionTarget : mTarget, matrix);
     }
 
+    private void draw(PhotoFrame target, float[] matrix) {
+        // Bind default FBO
+        GLES20.glBindFramebuffer(GLES20.GL_FRAMEBUFFER, 0);
+        GLESUtil.glesCheckError("glBindFramebuffer");
+
+        // Use our shader program
+        useProgram(0);
+
+        // Disable blending
+        GLES20.glDisable(GLES20.GL_BLEND);
+        GLESUtil.glesCheckError("glDisable");
+
+        // Apply the projection and view transformation
+        GLES20.glUniformMatrix4fv(mMVPMatrixHandlers[0], 1, false, matrix, 0);
+        GLESUtil.glesCheckError("glUniformMatrix4fv");
+
+        // Texture
+        FloatBuffer textureBuffer = target.getTextureBuffer();
+        textureBuffer.position(0);
+        GLES20.glVertexAttribPointer(mTextureCoordHandlers[0], 2, GLES20.GL_FLOAT, false, 0, textureBuffer);
+        GLESUtil.glesCheckError("glVertexAttribPointer");
+        GLES20.glEnableVertexAttribArray(mTextureCoordHandlers[0]);
+        GLESUtil.glesCheckError("glEnableVertexAttribArray");
+
+        // Position
+        FloatBuffer positionBuffer = target.getPositionBuffer();
+        positionBuffer.position(0);
+        GLES20.glVertexAttribPointer(mPositionHandlers[0], 2, GLES20.GL_FLOAT, false, 0, positionBuffer);
+        GLESUtil.glesCheckError("glVertexAttribPointer");
+        GLES20.glEnableVertexAttribArray(mPositionHandlers[0]);
+        GLESUtil.glesCheckError("glEnableVertexAttribArray");
+
+        // Set the input texture
+        int textureHandle = target.getTextureHandle();
+        GLES20.glActiveTexture(GLES20.GL_TEXTURE0);
+        GLESUtil.glesCheckError("glActiveTexture");
+        GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, textureHandle);
+        GLESUtil.glesCheckError("glBindTexture");
+        GLES20.glUniform1i(mTextureHandlers[0], 0);
+        GLESUtil.glesCheckError("glUniform1i");
+
+        // Draw
+        GLES20.glDrawArrays(GLES20.GL_TRIANGLE_STRIP, 0, 4);
+        GLESUtil.glesCheckError("glDrawElements");
+
+        // Disable attributes
+        GLES20.glDisableVertexAttribArray(mPositionHandlers[0]);
+        GLESUtil.glesCheckError("glDisableVertexAttribArray");
+        GLES20.glDisableVertexAttribArray(mTextureCoordHandlers[0]);
+        GLESUtil.glesCheckError("glDisableVertexAttribArray");
+    }
 }
