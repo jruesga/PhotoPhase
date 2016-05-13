a3313cdb817e4d68121d22a4e363468c925098af
Jorge Ruesga <jorge@ruesga.com>
2016-05-14 00:30:59 +0200

    Remove debug traces
    Signed-off-by: Jorge Ruesga <jorge@ruesga.com>

--- app/src/main/java/com/ruesga/android/wallpapers/photophase/transitions/ApertureTransition.java
+++ app/src/main/java/com/ruesga/android/wallpapers/photophase/transitions/ApertureTransition.java
@@ -69,7 +69,6 @@ public class ApertureTransition extends Transition {
     }
 
     private void draw(float[] matrix, float radius) {
-System.out.println("jrc:draw aperture");
         // Bind default FBO
         GLES20.glBindFramebuffer(GLES20.GL_FRAMEBUFFER, 0);
         GLESUtil.glesCheckError("glBindFramebuffer");
