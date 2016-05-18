bbf579b721f193256131805a9d7b55efb539f828
Jorge Ruesga <jorge@ruesga.com>
2016-05-18 23:17:04 +0200

    Remove ununsed imports
    Signed-off-by: Jorge Ruesga <jorge@ruesga.com>

--- app/src/main/java/com/ruesga/android/wallpapers/photophase/utils/GLESUtil.java
+++ app/src/main/java/com/ruesga/android/wallpapers/photophase/utils/GLESUtil.java
@@ -37,7 +37,6 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
 
-import javax.microedition.khronos.egl.EGL;
 import javax.microedition.khronos.egl.EGL10;
 import javax.microedition.khronos.egl.EGLContext;
 
